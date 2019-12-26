5af8781ed9173787bff709594e1acc550ab2c962
nefizik <timur24032002@gmail.com>
2019-12-26 21:10:15 +0300

    First commit
    Try to put this prjoect to github

---
+++ timetable.py
@@ -0,0 +1,238 @@
+import logging
+import telegram
+import sqlite3
+import datetime
+import hashlib
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# class_ = 'Idontknow'
+
+
+# dont forget to create list so that not call base a lot
+def classes_list():
+    # take classes from database and make the list
+    connection_to_database = sqlite3.connect('BOTSBASE.db')
+    database_cursor = connection_to_database.cursor()
+
+    cursor_execution_result = database_cursor.execute('select class_, letter from classes_ order by class_;').fetchall()
+    keyboard = [[]]
+
+    cursor_execution_result_size = len(cursor_execution_result)
+
+    i = 0
+    j = 0
+    while i < cursor_execution_result_size:
+        keyboard.append([])
+        while j < cursor_execution_result_size and cursor_execution_result[j][0] == cursor_execution_result[i][0]:
+            keyboard[i + 1].append(str(cursor_execution_result[j][0])
+                                   + cursor_execution_result[j][1])
+            j += 1
+        i += 1
+
+    connection_to_database.close()
+    return keyboard
+
+
+def weekday_list():
+    connection_to_database = sqlite3.connect('BOTSBASE.db')
+    database_cursor = connection_to_database.cursor()
+    cursor_execution_result = database_cursor.execute('select weekday from Weekdays order by id;').fetchall()
+    keyboard = [[]]
+    i = 0
+    j = 0
+    keyboard.append([])
+
+    for row in cursor_execution_result:
+        keyboard[i + 1].append(str(*cursor_execution_result[2 * i + j]))
+        j += 1
+        if j == 2:
+            j = 0
+            i += 1
+            keyboard.append([])
+
+    connection_to_database.close()
+    return keyboard
+
+
+def teachers_list():
+    # take teachers list from database and make the list
+    connection_to_database = sqlite3.connect('BOTSBASE.db')  # Подключаюсь к базе данных
+    database_cursor = connection_to_database.cursor()
+
+    cursor_execution_result = database_cursor.execute('select SNF from Teachers order by SNF;').fetchall()
+    teach_keyboard = []
+
+    for row in cursor_execution_result:
+        teach_keyboard.append(row)
+
+    # database_cursor.execute("INSERT INTO Teachers(SNF) VALUES('%name%')")
+    # database_cursor.execute('DELETE FROM Teachers WHERE id==23;')
+    # reply_markup = telegram.ReplyKeyboardRemove()
+
+    connection_to_database.close()  # Закрываю подключение к базе данных
+    return teach_keyboard
+
+
+def is_weekday(update, context):
+    a = weekday_list()
+    for i in range(0, len(a)):
+        for j in range(0, len(a[i])):
+            if update.message.text == a[i][j]:
+                return True
+    return False
+
+
+def is_student(update, context):
+    a = classes_list()
+    for i in range(0, len(a)):
+        for j in range(0, len(a[i])):
+            if update.message.text == a[i][j]:
+                return update.message.text
+    return False
+
+
+def is_teacher(update, context):
+    a = teachers_list()
+    for i in range(0, len(a)):
+        if update.message.text == a[i]:
+            return True
+    return False
+
+
+def class_selection_menu(update, context):
+    markup = telegram.ReplyKeyboardMarkup([['Я учитель']] + classes_list(), one_time_keyboard=True)
+    update.message.reply_text('Здравствуй!\nВыбери свой класс из списка ниже.', reply_markup=markup)
+
+
+def teacher_selection_menu(update, context):
+    teach_markup = telegram.ReplyKeyboardMarkup(teachers_list(), one_time_keyboard=True)
+    update.message.reply_text('Выберите свое имя в списке ниже.\nНе учитель? Нажмите /start чтобы начать сначала.',
+                              reply_markup=teach_markup)
+
+
+def weekday_selection_menu(update, context):
+    markup = telegram.ReplyKeyboardMarkup([['Сегодня'], ['Завтра']] + weekday_list() + [['Расписание звонков']],
+                                          one_time_keyboard=False)
+    update.message.reply_text('Успешно!\nВыбрать класс заново? /start', reply_markup=markup)
+
+
+# suspicious
+def students(update, context, class_):
+    # global class_
+    # class_ = update.message.text
+    print(class_)
+    weekday_selection_menu(update, context)
+
+
+def printing_for_students(update, context, class_):
+    print("pfs", class_)
+    connection_to_database = sqlite3.connect('BOTSBASE.db')
+    database_cursor = connection_to_database.cursor()
+    # global class_
+    if len(class_) == 3:
+        letter = class_[2]
+        digits = class_[0] + class_[1]
+    else:
+        letter = class_[1]
+        digits = class_[0]
+    # get id of class
+    uclass = database_cursor.execute(
+        f'select id from Classes_ where class_ == {int(digits)} and letter == "{letter}";').fetchone()
+    # get id of weekday
+    todays_weekday = database_cursor.execute(
+        f'select id from Weekdays where weekday == "{update.message.text}";').fetchone()
+
+    # get timetable for this weekday and class
+    timetable = database_cursor.execute(
+        f'select lesson, cab, lesson_number from main_timetable where weekday == {todays_weekday[0]} and class_ == {uclass[0]} order by lesson_number;').fetchall()
+
+    keyboard = ""
+
+    i = 0
+    les_num = 1
+    while i < len(timetable):
+        keyboard += f"{les_num})"
+        lesson_name = database_cursor.execute(
+            f'select lesson from Lessons where id=={timetable[i][0]};').fetchone()
+        if len(timetable) - i > 1:
+            if timetable[i][2] == timetable[i + 1][2]:
+                if timetable[i][0] != timetable[i + 1][0]:
+                    lesson_name2 = database_cursor.execute(
+                        f'select lesson from Lessons where id=={timetable[i + 1][0]}').fetchone()
+                    keyboard += f"{lesson_name[0]} ({timetable[i][1]}) / {lesson_name2[0]} ({timetable[i + 1][1]})\n"
+                    print(lesson_name)
+                    print(lesson_name2)
+                else:
+                    keyboard += f"{lesson_name[0]} ({timetable[i][1]}) / ({timetable[i + 1][1]})\n"
+                i += 2
+                les_num += 1
+                continue
+
+        keyboard += f"{lesson_name[0]} ({timetable[i][1]})\n"
+        i += 1
+        les_num += 1
+
+    connection_to_database.close()
+    update.message.reply_text(keyboard)
+
+
+def distributor(update, context):
+    # calling other functions go by text from human
+    if update.message.text == 'Я учитель':
+        teacher_selection_menu(update, context)
+    result_is_student = is_student(update, context)
+    if result_is_student:
+        students(update, context, result_is_student)
+    if is_teacher(update, context):
+        students(update, context)
+    if is_weekday(update, context):
+        printing_for_students(update, context, str(result_is_student))
+    # telegram.ext.commandhandler
+
+
+def main():
+    updater = Updater(token='884658566:AAG5l3pY4CacvQamPZBAJhF25NjUyQHnp4k', use_context=True)
+
+    # git start
+    # first commit
+    # try to save
+
+    # get the dispatcher to register handlers (обработчики)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', class_selection_menu))
+    dp.add_handler(CommandHandler('edit_timetable', head_teacher_loggining))
+
+    dp.add_handler(MessageHandler(filters=Filters.text, callback=distributor))
+    updater.start_polling()
+    updater.idle()
+
+
+# not using
+def is_the_password_correct(the_password):  # может использовать много памяти
+    return hashlib.pbkdf2_hmac('sha256', the_password.encode('UTF-8'), b'PTS', 50) == \
+           b'\x89@\xf5Hd\x1a9\xd7\xb1\x07\x03\x81\x07b\xe1\x80\xd4h`/\xde\x16\xd6\x95\x9fUh\x05\xd7\x991w'
+
+
+def head_teacher_loggining(update, context):
+    update.message.reply_text('Меню редактирования расписания.\nДля внесения изменений введите пароль.\n'
+                              'Попали сюда случайно? /start')
+
+    if is_the_password_correct(update.message.text):
+        update.message.reply_text('Успешно!!!')
+        # smth
+    else:
+        update.message.reply_text('НЕВЕРНЫЙ ПАРОЛЬ.\nПопробуйте еще раз')
+
+
+# not using
+
+if __name__ == '__main__':
+    main()
